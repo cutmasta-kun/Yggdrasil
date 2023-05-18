b3e9c256ee2d540853f92d2c23fb3a3500fe442a
cutmasta-kun <valerij.molodew@web.de>
2023-05-18 22:15:50 +0200

    Add uuid to messages schema; Datasette Plugin generates uuid if posting new datasets; Response of POST request point to the newly created dataset;

--- pkg/plugin-datasette/flask_server.py
+++ pkg/plugin-datasette/flask_server.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 import requests
 import json
 import logging
+import uuid
 
 # Configurate application
 logging.basicConfig(level=logging.INFO)
@@ -32,18 +33,64 @@ def openapi_spec():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def catch_all(path):
+    # Überprüfen Sie, ob der Pfad mit '.json' endet und fügen Sie es hinzu, wenn nicht
+    if not path.endswith('.json'):
+        path += '.json'
+
+    generated_uuid = None
+    if request.method == 'POST':
+        generated_uuid = uuid.uuid4()
+        data = request.get_json()
+        if data is not None:  # überprüfen, ob der Request Body leer ist
+            data['uuid'] = str(generated_uuid)
+        else:
+            data = {'uuid': str(generated_uuid)}
+        data = json.dumps(data)
+    else:
+        data = request.get_data()
+
+    params = request.args.to_dict() if request.method == 'GET' else {}
+
     response = requests.request(
         method=request.method,
         url=f"http://memory:8001/{path}",
         headers={key: value for (key, value) in request.headers if key != 'Host'},
-        data=request.get_data(),
+        params=params, 
+        data=data,
         allow_redirects=False)
 
-    headers = []
-    headers.append(('Transfer-Encoding', 'identity'))  # Disable chunked transfer encoding
+    headers = [('Transfer-Encoding', 'identity')]  # Disable chunked transfer encoding
 
-    response = app.response_class(response.content, response.status_code, headers)
-    logging.info(response)
+    # Check if the 'Location' header is in the response, and if so, add it to your own headers
+    if 'Location' in response.headers:
+        headers.append(('Location', f"{response.headers['Location']}{generated_uuid}"))
+    
+    # Assume the response is not JSON and set response_content accordingly
+    response_content = response.text
+
+    logging.info(response.headers.get('Content-Type'))
+
+    # Check if the response is JSON, and if so, change the 'redirect' field
+    if response.headers.get('Content-Type').startswith('application/json'):
+        try:
+            body = response.json()  # Convert the response body to a Python dict
+            logging.info(body)
+            if body.get("ok", False) and body.get("message", "").endswith(" inserted") and 'redirect' in body:
+                redirect_base = body['redirect']
+                logging.info(body.get('redirect'))
+                body['redirect'] = f"{redirect_base}{generated_uuid}"
+                logging.info(body.get('redirect'))
+                response_content = json.dumps(body)
+        except ValueError:
+            logging.error(ValueError)
+            pass  # Not a JSON response; do nothing
+
+    response = app.response_class(
+        response_content,
+        status=response.status_code,
+        headers=headers,
+        mimetype='application/json')
+
     return response
 
 if __name__ == "__main__":
