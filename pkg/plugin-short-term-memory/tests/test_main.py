ca3e3450920e876dc731c1f432f6486061b7f8a9
cutmasta-kun <valerij.molodew@web.de>
2023-11-13 13:28:54 +0100

    aider: Add a test for the endpoint `/get_memory` that mocks the return of the service and tests the endpoint without providing a UUID.
    # Aider chat conversation:
    
    USER: Add a Test for the endpoint `/get_memory`. We can change the `MEMORY_HOST` env, so let's mock the return of the service and test the endpoint without providing a uuid first
    
    ASSISTANT: To add a test for the `/get_memory` endpoint, we will need to modify the `test_main.py` file in the `pkg/plugin-short-term-memory/tests` directory. We will use the `pytest` framework and the `fastapi.testclient` module to create a test client for our FastAPI application. We will also use the `unittest.mock` module to mock the `MemoryClient` class's `get_action` method, which is used to interact with the memory service.
    
    Here's how we can add the test:
    
    1. Import the necessary modules and classes at the top of the `test_main.py` file.
    2. Create a fixture for the FastAPI test client.
    3. Write a test function that mocks the `MemoryClient.get_action` method to return a predefined response.
    4. Use the test client to make a request to the `/get_memory` endpoint and assert the expected response.
    
    Let's implement these steps:
    
    <source>python
    pkg/plugin-short-term-memory/tests/test_main.py
    <<<<<<< SEARCH
    import pytest
    
    def test_example():
        assert True  # This is a simple test to ensure pytest is working
    =======
    from fastapi.testclient import TestClient
    from unittest.mock import patch
    from api_server import app
    import pytest
    from memory_client import MemoryClient
    
    @pytest.fixture
    def client():
        with TestClient(app) as c:
            yield c
    
    def test_get_memory_without_uuid(client):
        # Mock the return value of the MemoryClient's get_action method
        with patch.object(MemoryClient, 'get_action', return_value=({"ok": True, "message": "Success", "data": []}, 200, {})) as mock_get:
            # Make a request to the /get_memory endpoint
            response = client.get("/get_memory")
            # Assert that the response is as expected
            assert response.status_code == 200
            assert response.json() == {"ok": True, "message": "Success", "data": []}
            # Assert that the get_action method was called once
            mock_get.assert_called_once()
    >>>>>>> REPLACE
    </source>
    
    This test will mock the `MemoryClient.get_action` method to return a successful response with an empty data list when no UUID is provided. It then asserts that the response from the `/get_memory` endpoint matches the mocked response and that the `get_action` method was called exactly once.

--- pkg/plugin-short-term-memory/tests/test_main.py
+++ pkg/plugin-short-term-memory/tests/test_main.py
@@ -1,4 +1,21 @@
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from api_server import app
 import pytest
+from memory_client import MemoryClient
 
-def test_example():
-    assert True  # This is a simple test to ensure pytest is working
+@pytest.fixture
+def client():
+    with TestClient(app) as c:
+        yield c
+
+def test_get_memory_without_uuid(client):
+    # Mock the return value of the MemoryClient's get_action method
+    with patch.object(MemoryClient, 'get_action', return_value=({"ok": True, "message": "Success", "data": []}, 200, {})) as mock_get:
+        # Make a request to the /get_memory endpoint
+        response = client.get("/get_memory")
+        # Assert that the response is as expected
+        assert response.status_code == 200
+        assert response.json() == {"ok": True, "message": "Success", "data": []}
+        # Assert that the get_action method was called once
+        mock_get.assert_called_once()
